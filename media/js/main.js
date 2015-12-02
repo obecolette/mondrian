185728adeb6395b3b844bbf51c2a62523fbe1568
obecolette <rebecca.reese@gmail.com>
2015-12-02 16:33:48 -0500

    studio update and scroll js

---
+++ media/js/main.js
@@ -0,0 +1,22 @@
+TweenLite.to(window, 2, {scrollTo:{y:400}, ease:Power2.easeOut});
+
+
+finger down
+swipe
+up
+console log
+
+window.querySelectorAll('load', function(){
+  var section = document.getElementsByTagName('section')
+  var hidetimer = null
+  swipedetect(section, function(swipedir){
+    if (swipedir != 'none'){
+      clearTimeout(hidetimer)
+      var swipe = swipedir + 'swipe-icon.jpg' // naming convention is "leftarrow.png", "rightarrow.png" etc
+      inner.style.background = 'transparent url(' + bgimage + ') center center no-repeat'
+      hidetimer = setTimeout(function(){ // reset background image after 1 second
+          inner.style.background = ''
+      }, 1000)
+    }
+  })
+}, false)
