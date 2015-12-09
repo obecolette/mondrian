b31b86049c8ba0996d1c4e086ac3bc67941e504c
obecolette <rebecca.reese@gmail.com>
2015-12-09 01:25:08 -0500

    draggable studio feature

--- media/js/createStudio.js
+++ media/js/createStudio.js
@@ -1,1 +1,34 @@
-$('#widget').draggable();
+// var $clone_instance = $('.square').clone();
+
+// $clone_instance.addClass('square_instance');
+
+$(function() {
+  $('#widget').draggable();
+
+  $( '.square_instance' ).draggable({
+    revertDuration: 500,
+    appendTo: $(window),
+    snapMode: "inner",
+    snapTolerence: 100
+
+  });
+
+  $( '.frame' ).droppable({
+    activeClass: "ui-state-default",
+    hoverClass: "ui-state-hover",
+    drop: function( event, ui ) {
+      console.log('this');
+      $( this )
+        .addClass( "ui-state-highlight" )
+        .find( "p" )
+          .addClass( ".tap-icon" );
+    }
+  });
+});
+
+// for each static square there is an equiv draggable square on top
+// .draggable
+// when clicked the staic square will clone itself to initial position
+// draggable clicked square will either place in droppable or die
+
+// if an already placed draggable square is reclicked
