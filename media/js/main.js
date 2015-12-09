c992647a18bd75bd33cecb976238494cf09cf3e7
Bdtzau <user@users-MacBook-Pro-2.local>
2015-12-09 01:23:49 -0500

    Scroll!!!

--- media/js/main.js
+++ media/js/main.js
@@ -1,17 +1,57 @@
 (function() {
 
-  var moveToSection = function () {
-    var scrollSection = $(window).scrollLeft()/900;
-    console.log("scrollSection: " + scrollSection);
-    TweenMax.to($(window), 0.5, {scrollTo: {x: Math.round(scrollSection)*900 + "px"} });
-    window.removeEventListener("touchend", function () {window.setTimeout(moveToSection, 1000);}, false);
+	$.fn.scrollEnd = function(callback, timeout) {          
+		$(this).scroll(function(){
+			var $this = $(this);
+			if ($this.data('scrollTimeout')) {
+				clearTimeout($this.data('scrollTimeout'));
+			}
+			$this.data('scrollTimeout', setTimeout(callback,timeout));
+		});
+	};
 
-  };
-  window.addEventListener("touchstart", function () {
-    window.addEventListener("touchend", function () {window.setTimeout(moveToSection, 1000);}, false);
-  }, false);
+	$(window).bind('touchstart', function (event) {
 
-  window.addEventListener("touchmove", function () {
-    console.log($(window).scrollLeft());
-  }, false);
+		waitForTouchEnd();
+	});
+
+	var waitForTouchEnd = function () {
+		$(window).scrollEnd( function (innerEvent) {
+
+			
+				// console.log("scrollSection: " + scrollSection);
+
+				var touchStop = window.setTimeout(function () {
+
+				var scrollSection = Math.round($(window).scrollLeft()/900);
+
+				var storedOffset = $('body > section:eq(' + scrollSection + ')').offset();
+
+				TweenMax.to($(window), 0.5, {scrollTo: {x: storedOffset.left} });
+				// $(window).unbind(innerEvent);
+			}, 500);
+
+			// $(window).bind('touchstart', function (innerInnerEvent) {
+				// $(window).unbind(innerInnerEvent);
+				// window.clearTimeout(touchStop);
+				// $(window).unbind(innerEvent);
+			// });
+			
+		});
+	};
+
+	// var moveToSection = function () {
+	//   var scrollSection = $(window).scrollLeft()/900;
+	//   console.log("scrollSection: " + scrollSection);
+	//   TweenMax.to($(window), 0.5, {scrollTo: {x: Math.round(scrollSection)*900 + "px"} });
+	//   window.removeEventListener("touchend");
+
+	// };
+	// window.addEventListener("touchstart", function () {
+	//   window.addEventListener("touchend", function () {window.setTimeout(moveToSection, 1000);}, false);
+	// }, false);
+
+	// window.addEventListener("touchmove", function () {
+	//   console.log($(window).scrollLeft());
+	// }, false);
 })();
