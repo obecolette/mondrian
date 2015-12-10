1c1a338daf09524145fd5447f7de8d76ce57aad3
obecolette <rebecca.reese@gmail.com>
2015-12-09 21:43:34 -0500

    create studio clones

--- media/js/createStudio.js
+++ media/js/createStudio.js
@@ -14,12 +14,10 @@ $(function() {
     snapTolerence: 100,
     start: function(event, ui) {
       console.log("dragging started");
+      $(ui.helper).clone(true).insertAfter( ui.helper );
+
     }
   });
-
-  // var $clone_instance = $('.square').clone();
-
-  // $clone_instance.addClass('square_instance');
 
   $( '.side-back' ).droppable({
     drop: function( event, ui ) {
@@ -34,8 +32,6 @@ $(function() {
 
 
 // for each static square there is an equiv draggable square on top
-// .draggable
 // when clicked the staic square will clone itself to initial position
 // draggable clicked square will either place in droppable or die
-
 // if an already placed draggable square is reclicked it will not die, be draggable, or have the icon option to delete.
